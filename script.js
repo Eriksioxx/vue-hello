20a2012c422e443bdce75aba77780fb077c89408
erik schievenin <erik.schievenin@gmail.com>
2022-03-25 09:29:19 +0100

    Exercise complete

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('JS OK!');
+
+const app = new Vue(
+    {
+        el: '#vue-app',
+        data: {
+            testo: 'Hello Vue World',
+            img: "https://picsum.photos/200/300",
+            color: 'blue',
+        }
+    }
+);
+
+
